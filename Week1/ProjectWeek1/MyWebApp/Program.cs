dff4710e39bcece550636b15de9802be3f15ac84
Satyam Choubey <163556915+Satyam7371@users.noreply.github.com>
2025-05-25 23:11:39 +0530

    Add files via upload

---
+++ Week1/ProjectWeek1/MyWebApp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from MyWebApp!");
+        }
+    }
+}
